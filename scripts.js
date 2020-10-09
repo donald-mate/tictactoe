21d13f70a0e39c787be4ea7dd04be65908fb80c7
Donald <donald.mate@gmail.com>
2020-10-09 17:01:10 -0400

    placed fillBoard function in a module

--- scripts.js
+++ scripts.js
@@ -1,47 +1,48 @@
-// const gameboard = (() => {
-//   let markerArray = ["X", "O", "O", "O", "X", "O", "X", "O", "X"];
-//   return { markerArray };
+const gameBoard = (() => {
+  const aOne = document.getElementById("a-one");
+  const aTwo = document.getElementById("a-two");
+  const aThree = document.getElementById("a-three");
+  const bOne = document.getElementById("b-one");
+  const bTwo = document.getElementById("b-two");
+  const bThree = document.getElementById("b-three");
+  const cOne = document.getElementById("c-one");
+  const cTwo = document.getElementById("c-two");
+  const cThree = document.getElementById("c-three");
+  const boardArray = [aOne, aTwo, aThree, bOne, bTwo, bThree, cOne, cTwo, cThree];
+  let markerArray = ["X", "O", "O", "O", "X", "O", "X", "O", "X"];
+  const fillBoard = () => {
+    for (let i = 0; i <= boardArray.length; i++) {
+    let marker = markerArray[i];
+    let display = document.createTextNode(marker);
+    boardArray[i].appendChild(display);
+  }
+  }  
+  return { fillBoard };
+})();
+// const displayBoard = (() => {
+
 // })();
-// const displayController = (() => {
-//   let turn = 1;
-//   const board = document.getElementbyClassName("board");
-//   const fillBoard = () => {
-//     for (let i = 0; i <= board.length; i++) {
-//       board[i].innerHTML = gameboard.markerArray[i];
-//     }
-//   };
-//   return { fillBoard };
-// })();
-const playerCreator = (name, marker) => {
-  return { name, marker };
-};
-const player1 = playerCreator("Donald", "X");
-const player2 = playerCreator("Maribel", "O");
-// const game = displayController();
-// game.fillBoard();
-const aOne = document.getElementById("a-one");
-const aTwo = document.getElementById("a-two");
-const aThree = document.getElementById("a-three");
-const bOne = document.getElementById("b-one");
-const bTwo = document.getElementById("b-two");
-const bThree = document.getElementById("b-three");
-const cOne = document.getElementById("c-one");
-const cTwo = document.getElementById("c-two");
-const cThree = document.getElementById("c-three");
-const boardArray = [aOne, aTwo, aThree, bOne, bTwo, bThree, cOne, cTwo, cThree];
 
-let markerArray = ["X", "O", "O", "O", "X", "O", "X", "O", "X"];
-for (let i = 0; i <= boardArray.length; i++) {
-  let marker = markerArray[i];
-  let display = document.createTextNode(marker);
-  boardArray[i].appendChild(display);
-}
+// PLAYER CREATION
+// const playerCreator = (name, marker) => {
+//   return { name, marker };
+// };
+// const player1 = playerCreator("Donald", "X");
+// const player2 = playerCreator("Maribel", "O");
+gameBoard.fillBoard();
 
 
 
 
-// let display = document.createTextNode("X")
-// aOne.appendChild(display);
 
 
-aOne.addEventListener("click", function () {});
+// EVENT LISTENERS
+// aOne.addEventListener("click", function () {});
+// aTwo.addEventListener("click", function () {});
+// aThree.addEventListener("click", function () {});
+// bOne.addEventListener("click", function () {});
+// bTwo.addEventListener("click", function () {});
+// bThree.addEventListener("click", function () {});
+// cOne.addEventListener("click", function () {});
+// cTwo.addEventListener("click", function () {});
+// cThree.addEventListener("click", function () {});
