42a4ba0fbbb98bc88214b9f5228f09d9bc7a74a8
Donald <donald.mate@gmail.com>
2020-10-09 11:52:07 -0400

    first attempts at logic in scripts

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
-const gameboard = ((array) => {})();
-const displayController = ((array) => {})();
-const playerCreator = (name, choice) => {};
+const gameboard = (() => {
+  let markerArray = ["X", "O", "O", "O", "X", "O", "X", "O", "X"];
+  return { markerArray };
+})();
+const displayController = (() => {
+  let turn = 1;
+  const board = document.getElementbyClassName("board");
+  const fillBoard = () => {
+    for (let i = 0; i <= board.length; i++) {
+      board[i].innerHTML = gameboard.markerArray[i];
+    }
+  };
+  return { fillBoard };
+})();
+const playerCreator = (name, marker) => {
+  return { name, marker };
+};
+const player1 = playerCreator("Donald", "X");
+const player2 = playerCreator("Maribel", "O");
+const game = displayController();
+game.fillBoard();
+
+let mark = document.createTextNode("X");
+aOne.appendChild(mark);
+
+const aOne = document.getElementById("a-one");
+
+aOne.addEventListener("click", function () {});
